fff027b033f13efb273d98eb94d2e1c5dc451962
Lankonyue <lankonyue@foxmail.com>
2025-11-10 21:36:08 +0800

    update hexo commands

--- environment_setup/deploy_your_hexo_blog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# 安装 git　部署插件
-npm install hexo-deployer-git --save
---
+++ environment_setup/hexo/03_install_hexo_tools.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 安装 git　部署插件
+npm install hexo-deployer-git --save
+
+# 安装搜索
+npm install hexo-generator-search --save
+
+# 文章字数统计
+npm i --save hexo-wordcount
+
+# emoji 支持
+npm install hexo-filter-github-emojis --save
+
+# RSS feed 支持
+npm install hexo-generator-feed --save
+
+
